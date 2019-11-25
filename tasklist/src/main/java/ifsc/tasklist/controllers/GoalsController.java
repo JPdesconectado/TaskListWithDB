333a87c8b4f5c9d73687b17859189d053509a354
Jhony Pereira <40416490+JPdesconectado@users.noreply.github.com>
2019-11-25 03:33:49 -0300

    Atualização
    Mudando o projeto para java 12 em vez de 11.

--- tasklist/src/main/java/ifsc/tasklist/controllers/GoalsController.java
+++ tasklist/src/main/java/ifsc/tasklist/controllers/GoalsController.java
@@ -3,14 +3,9 @@ package ifsc.tasklist.controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 import com.jfoenix.controls.JFXButton;
-import ifsc.tasklist.Task;
-import ifsc.tasklist.TaskDAO;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
 import javafx.scene.control.ProgressBar;
-import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 public class GoalsController implements Initializable{
@@ -25,27 +20,7 @@ public class GoalsController implements Initializable{
 	@FXML
 	JFXButton btVoltar;
 	
-	ObservableList<Task> teste;
-	
 	public void update() {
-		TaskDAO dao = new TaskDAO();
-		double j = 0.0;
-		teste = (ObservableList<Task>) dao.getAll();
-		for(int i = 0; i < teste.size(); i++) {
-
-			j += 0.1;
-			proTasks.setProgress(j);
-			
-			if(teste.size() == 4 && check) {
-				Alert alert = new Alert(AlertType.WARNING);
-				alert.setTitle("Temos Tarefas para Hoje!");
-				alert.setHeaderText("Título da Tarefa: ");
-				alert.setContentText("Batata");
-				alert.showAndWait();
-				check = false;
-				break;
-			}
-		}
 		
 	}
 	@Override
